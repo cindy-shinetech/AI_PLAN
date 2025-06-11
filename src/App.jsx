93e28423f31d70da544a3bd683f38010da4a0ce9
ZhenxingXiao <xiaozx@shinetechsoftware.com>
2025-06-11 17:42:41 +0800

    import cagnito

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
-import { CssBaseline, Container } from '@mui/material'
+import { CssBaseline, Container, Button } from '@mui/material'
 import UploadPage from './pages/UploadPage'
 import RecognitionResults from './pages/RecognitionResults'
+import { useAuth } from 'react-oidc-context'
+import { PageContainer } from './components/Layout/PageContainer'
+import { Loading } from './components/Loading'
 
 function App() {
+  const auth = useAuth();
+  if (auth.isLoading) {
+    return <div className='h-full w-full flex items-center justify-center'>
+      <Loading />
+    </div>
+  }
+  if (auth.error) {
+    return <div>Error: {auth.error.message}</div>
+  }
+  if(auth.isAuthenticated) {
+    return (
+      <Router>
+        <CssBaseline />
+        {/* <Container maxWidth="lg"> */}
+          <Routes> 
+            <Route path="/" element={<UploadPage />} />
+            <Route path="/results" element={<RecognitionResults />} /> 
+          </Routes>
+        {/* </Container> */}
+      </Router>
+    )
+  } else {
+    auth.signinRedirect();
+  }
   return (
-    <Router>
-      <CssBaseline />
-      {/* <Container maxWidth="lg"> */}
-        <Routes> 
-          <Route path="/" element={<UploadPage />} />
-          <Route path="/results" element={<RecognitionResults />} /> 
-        </Routes>
-      {/* </Container> */}
-    </Router>
+    <></>
   )
+  
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { AuthProvider } from 'react-oidc-context';
  
- 
+ const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_qlXqYDUBU",
+  client_id: "5o5ef6kl1bmb37t41kvqmpe7ek",
+  redirect_uri: "http://localhost:5173",
+  response_type: "code",
+  scope: "email openid phone",
+};
  
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
